6d46fb55547860efd6559a9aa92379e772eff837
cpshibinoppo <cpshibinoppo@gmail.com>
2022-07-15 18:14:27 +0530

    office over

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {  Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { Address } from 'ngx-google-places-autocomplete/objects/address';
 type PaneType = 'left' | 'right';
 @Component({
@@ -7,17 +7,22 @@ type PaneType = 'left' | 'right';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  private app = document.getElementById("sampleid");
+
   title = 'Swiggy';
   options: any = {
-    componentRestrictions: { country: 'IN' }
-  }
-
+    componentRestrictions: { country: 'IN' },
+  };
+  opened = false;
   handleAddressChange(address: Address) {
-    console.log(address.formatted_address)
-    console.log(address.geometry.location.lat())
-    console.log(address.geometry.location.lng())
+    console.log(address.formatted_address);
+    console.log(address.geometry.location.lat());
+    console.log(address.geometry.location.lng());
   }
-  getsignuppage(){
-    console.log("Get signup page");
+  getsignuppage() {
+    console.log('Get signup page');
+
+      console.log(this.app);
+     console.log("color");
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatIconModule} from '@angular/material/icon';
-import { GooglePlaceModule } from "ngx-google-places-autocomplete";
+import { MatIconModule } from '@angular/material/icon';
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatIconModule,
-    GooglePlaceModule
+    GooglePlaceModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
