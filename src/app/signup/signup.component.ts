c700e18e98e0d252f86c5b7369fc345f4c403c03
cpshibinoppo <cpshibinoppo@gmail.com>
2022-07-18 18:08:42 +0530

    add

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,33 +3,40 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss'],
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
   @ViewChild('username', { static: true }) usernameElement!: ElementRef;
   userdata: string = '';
   myusername: string = '';
   ElementRef: any;
-
-  constructor(usernameElement: ElementRef) {
-    this.usernameElement = usernameElement;
-  }
   car = false;
-  ngOnInit(): void {}
-  onFocus() {
+  carr = false;
+
+  ngOnInit(): void {
+    console.log(this.usernameElement.nativeElement.name);
+  }
+  onFocus(one: any) {
+    console.log(one);
     this.myusername = this.usernameElement.nativeElement.value;
-    if (this.myusername == '') {
-      this.car = true;
-      console.log('on');
+    if (one == 'one') {
+      if (this.myusername == '') {
+        this.car = true;
+        console.log('on');
+      }
+    }else if(one == 'two'){
+      this.carr = true;
     }
   }
   focusOut() {
     this.myusername = this.usernameElement.nativeElement.value;
     if (this.myusername == '') {
       this.car = false;
+      this.carr = false;
       console.log('outoff');
     } else {
       this.car = true;
+      this.carr = true;
       console.log('outon');
     }
   }
