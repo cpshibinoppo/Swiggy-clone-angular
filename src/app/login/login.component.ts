1ad045e4dd3a2d7ebd2791a3f9b959ff350fec64
cpshibinoppo <cpshibinoppo@gmail.com>
2022-07-21 09:33:29 +0530

    loging erroe

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,63 +6,40 @@ import { FormBuilder, Validators } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   constructor(private fb: FormBuilder) {}
   sub = false;
   sam = false;
-  pass = false;
   loginForm = this.fb.group({
     phonenumber: [
       '',
       [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')],
-    ],
-    password: [''],
+    ]
   });
   get f() {
     return this.loginForm.controls;
   }
-  onsubmit(par :any) {
+  onsubmit(par: any) {
     this.sub = true;
-    if (par == 'one') {
       if (this.f.phonenumber.value == '') {
         this.sam = false;
       }
-    } else if (par == 'two') {
-      if (this.f.password.value == '') {
-        this.pass = false;
-      }
-    } else {
+    else {
       this.sam = true;
-      this.pass = true;
-
     }
   }
   onFocus(par: any) {
-    console.log(this.f.phonenumber.value);
-    if (par == 'one') {
-      if (this.f.phonenumber.value == '') {
-        this.sam = true;
-      }
-    } else if (par == 'two') {
-      if (this.f.password.value == '') {
-        this.pass = true;
-      }
+    if (this.f.phonenumber.value == '') {
+      this.sam = true;
     }
   }
   focusOut(par: any) {
     this.sub = true;
-    if (par == 'one') {
-      if (this.f.phonenumber.value == '') {
-        this.sam = false;
-      }
-    } else if (par == 'two') {
-      if (this.f.password.value == '') {
-        this.pass = false;
-      }
+
+    if (this.f.phonenumber.value == '') {
+      this.sam = false;
     } else {
       this.sam = true;
-      this.pass = true;
     }
   }
-  ngOnInit(): void {}
 }
