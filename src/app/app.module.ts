64f1e9ef8b6ae79eab72a30f0941111ef397311e
cpshibinoppo <cpshibinoppo@gmail.com>
2022-07-24 12:29:51 +0530

    add pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './pages/signup/signup.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 @NgModule({
   declarations: [AppComponent, SignupComponent, LoginComponent],
   imports: [
