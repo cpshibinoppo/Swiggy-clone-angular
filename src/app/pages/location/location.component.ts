02c50f5c15cf867df6c96f823c765d5892f8b5c7
cpshibinoppo <cpshibinoppo@gmail.com>
2022-08-01 11:19:12 +0530

    location stayling

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,18 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
-  styleUrls: ['./location.component.css']
+  styleUrls: ['./location.component.css'],
 })
 export class LocationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  showlist(){
-
-  }
-
+  constructor() {}
+  toggle = false;
+  ngOnInit(): void {}
 }
