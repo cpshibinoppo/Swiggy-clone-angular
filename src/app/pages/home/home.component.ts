939081299ed5d045259f2b94c3a563f15705476e
cpshibinoppo <cpshibinoppo@gmail.com>
2022-08-20 10:28:30 +0530

    add

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit(): void {
+export class HomeComponent implements OnInit {
+ clickedElement:any;
+  constructor() { }
+  ngOnInit(): void {}
+  onButtonGroupClick($event:any){
+     this.clickedElement = $event.target || $event.srcElement;
+    let isCertainButtonAlreadyActive = this.clickedElement.parentElement.querySelector(".active");
+    if( this.clickedElement.nodeName === "BUTTON" ) {
+      if( isCertainButtonAlreadyActive) {
+          isCertainButtonAlreadyActive.classList.remove("active");
+      }
+      this.clickedElement.className += " active";
+    }
+
   }
 
 }
