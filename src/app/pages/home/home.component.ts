e6ed42b3709ff5145967f52d707a574afbaae25b
cpshibinoppo <cpshibinoppo@gmail.com>
2022-08-22 11:34:19 +0530

    card sub start

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,47 +7,22 @@ import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular
 })
 
 export class HomeComponent implements OnInit {
-    isSticky: boolean = false;
-
-
   add = false;
  clickedElement:any;
+ checkid:any
   constructor() { }
 
   ngOnInit(): void {
-    window.addEventListener("scroll", this.checkScroll, true);
   }
-  ngOnDestroy() {
-    window.removeEventListener("scroll", this.checkScroll, true);
-  }
-
-  checkScroll(event:any) {
-    console.log(window.pageYOffset)
-         if (window.pageYOffset > 550) {
-       let element = document.querySelector('.closediv');
-       element?.classList.add('sticky');
-     } else {
-      let element = document.querySelector('.closediv');
-        element?.classList.remove('sticky');
-     }
-  }
-
-  // stick scroll
-  // @HostListener('window:scroll', ['$event'])
-  // onWindowScroll() {
-  //    console.log("hi")
-    //  if (window.pageYOffset > 550) {
-    //    let element = document.querySelector('.closediv');
-    //    element?.classList.add('sticky');
-    //  } else {
-    //   let element = document.querySelector('.closediv');
-    //     element?.classList.remove('sticky');
-    //  }
-  // }
-  // end
   onButtonGroupClick($event:any){
+
      this.clickedElement = $event.target || $event.srcElement;
     let isCertainButtonAlreadyActive = this.clickedElement.parentElement.querySelector(".active");
+    if(this.clickedElement.id == ''){
+      this.checkid = 'rele'
+    }else{
+      this.checkid = this.clickedElement.id
+    }
     if( this.clickedElement.nodeName === "BUTTON" ) {
       if( isCertainButtonAlreadyActive) {
           isCertainButtonAlreadyActive.classList.remove("active");
@@ -58,7 +33,5 @@ export class HomeComponent implements OnInit {
   }
 
 }
-function checkScroll() {
-  throw new Error('Function not implemented.');
-}
+
 
