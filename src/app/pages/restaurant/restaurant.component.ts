bbc466d860c2f4af74e74dde715d8d49450d7f38
cpshibinoppo <cpshibinoppo@gmail.com>
2022-08-26 16:09:34 +0530

    add slider in restaurant page

--- src/app/pages/restaurant/restaurant.component.ts
+++ src/app/pages/restaurant/restaurant.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant.component.scss']
 })
 export class RestaurantComponent implements OnInit {
+  clickedElement:any;
+  checkid:any
+  full = false
+  demo = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  scrollToElement(targetname:any,$event:any): void {
+    $event.currentTarget.style.color = 'red';
+    // console.log($event.currentTarget);
+    targetname.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+  onButtonGroupClick($event:any){
+    this.clickedElement = $event.target || $event.srcElement;
+   let btnAlreadyActive = this.clickedElement.parentElement.querySelector(".active");
+   console.log("btn"+btnAlreadyActive)
+   if(this.clickedElement.id == ''){
+     this.checkid = 'rele'
+   }else{
+     this.checkid = this.clickedElement.id
+   }
+   if( this.clickedElement.nodeName === "BUTTON" ) {
+     if(btnAlreadyActive) {
+      btnAlreadyActive.classList.remove("active");
+     }
+     this.clickedElement.className += " active";
+   }
 
-}
+ }
+ };
