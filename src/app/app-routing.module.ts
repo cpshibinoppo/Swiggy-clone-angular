2a5ebbe590751050b487f0ce9a0d172fd91a9048
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-03 12:30:48 +0530

    barke

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { HomeComponent } from './pages/home/home.component';
+import { OffersComponent } from './pages/offers/offers.component';
 import { RestaurantComponent } from './pages/restaurant/restaurant.component';
 import { SearchComponent } from './pages/search/search.component';
 import { NotloginComponent } from './static/notlogin/notlogin.component';
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'home/restaurant',component:RestaurantComponent},
   {path:'home/cart',component:CartComponent, data: {con: 'SECURE CHECKOUT'}},
-  {path:'home/search',component:SearchComponent}
+  {path:'home/search',component:SearchComponent},
+  {path:'home/offers',component:OffersComponent}
 ];
 
 @NgModule({
--- src/app/pages/offers/offers.component.ts
+++ src/app/pages/offers/offers.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
-  styleUrls: ['./offers.component.css']
+  styleUrls: ['./offers.component.css'],
 })
 export class OffersComponent implements OnInit {
-
-  constructor() { }
+  offershow = false;
+  constructor() {}
 
   ngOnInit(): void {
+    $('#rest').addClass('btnadd');
   }
-
+  restfun(id: any) {
+    this.offershow = false;
+    if (id == 'rest') {
+      $('#rest').addClass('btnadd');
+      $('#paym').removeClass('btnadd');
+      $('#line').removeClass('lineadd');
+    } else if (id == 'paym') {
+      this.offershow = true;
+      $('#rest').removeClass('btnadd');
+      $('#paym').addClass('btnadd');
+      $('#line').addClass('lineadd');
+    }
+  }
 }
