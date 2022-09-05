45de04da8131b3b041771421446c92292877a4e7
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-05 18:06:15 +0530

    today start

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -8,6 +8,12 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  wallets = 'wallets';
+  cardnum = false;
+  checkboxshow = true;
+  cardimgname: any;
+  textcard: any;
+  nocon = true;
 usernavbartitle:any
   constructor(private route: ActivatedRoute) { }
 
@@ -15,6 +21,31 @@ usernavbartitle:any
     this.route.data.subscribe((data) => {
       this.usernavbartitle = data;
     });
+  }
+  onFocus(name: any) {
+    this.textcard = name;
+  }
+  focusOut(con: any) {
+    if (con == '') {
+      this.textcard = false;
+    }
+  }
+  resetdropdown(a: any) {
+    var canger;
+    canger = document.getElementById('sm');
+    canger!.innerHTML = 'Other banks';
+
+  }
+  activefun(btnname: any) {
+    document.querySelector('.active')!.classList.remove('active');
+    btnname.classList.add('active');
+  }
+  addnum = 1;
+  zero = true;
+
+  zerocheck() {
+    if (this.addnum < 1) {
+    }
   }
 
 }
