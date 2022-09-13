ec9ed31a8bfe46a7fe730fe265449eac20d7fc1b
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-13 09:34:10 +0530

    add some forter

--- Admin/src/app/app.module.ts
+++ Admin/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AnalyticsComponent } from './pages/analytics/analytics.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    AnalyticsComponent,
+    SignUpComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
