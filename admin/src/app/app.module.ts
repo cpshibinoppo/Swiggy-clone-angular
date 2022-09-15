17914e9eb309755cb4529fc470921fc67c498794
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-15 18:20:16 +0530

    add somr style

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogingComponent } from './pages/loging/loging.component';
-import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogingComponent,
-    SignupComponent
+    LogingComponent
   ],
   imports: [
     BrowserModule,
