b55e75cc3302299d1ad76aad2a784368dcdad8f2
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-26 10:07:54 +0530

    add some folder

--- Admin/src/app/app.module.ts
+++ Admin/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { TopbarComponent } from './layout/topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
